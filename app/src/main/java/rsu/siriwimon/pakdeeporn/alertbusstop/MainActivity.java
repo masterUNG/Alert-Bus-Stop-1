ea2c5bacfcecdc0dd3f87b891ff9bafd24588156
Dao <nubdow_1994@hotmail.com>
2016-10-31 13:59:56 +0700

    Onclick on Bus Stop

--- app/src/main/java/rsu/siriwimon/pakdeeporn/alertbusstop/MainActivity.java
+++ app/src/main/java/rsu/siriwimon/pakdeeporn/alertbusstop/MainActivity.java
@@ -1,13 +1,45 @@
 package rsu.siriwimon.pakdeeporn.alertbusstop;
 
+import android.media.MediaPlayer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ListView;
 
 public class MainActivity extends AppCompatActivity {
+
+    // Explicit
+    private ListView listView;
+    private Button button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
-}
+        //Bind Widget การผูกตัวแปร
+        listView = (ListView) findViewById(R.id.livBusStop);
+        button = (Button) findViewById(R.id.button);
+
+        //Button controller
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+               mySoundEfect(R.raw.add_bus1);
+            }// onClick
+        });
+
+    }// Main Medthod
+
+    private void mySoundEfect(int intSound) {
+        MediaPlayer mediaPlayer = MediaPlayer.create(getApplicationContext(),intSound);
+        mediaPlayer.start(); //ทำการร้อง
+
+        mediaPlayer.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
+            @Override
+            public void onCompletion(MediaPlayer mediaPlayer) {
+                mediaPlayer.release(); // คืนหน่วยความจำ
+            }
+        });
+    } // mySoundEfect
+}// Main class
