160990986ca8605024ea10a5d665de489df31ff8
masterUNG <phrombutr@gmail.com>
2017-02-27 15:32:25 +0700

    get My Location OK

--- app/src/main/java/rsu/siriwimon/pakdeeporn/alertbusstop/MainActivity.java
+++ app/src/main/java/rsu/siriwimon/pakdeeporn/alertbusstop/MainActivity.java
@@ -3,8 +3,13 @@ package rsu.siriwimon.pakdeeporn.alertbusstop;
 import android.content.Intent;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
+import android.location.Criteria;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.media.MediaPlayer;
 import android.os.Bundle;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
@@ -18,6 +23,10 @@ public class MainActivity extends AppCompatActivity {
     private ListView listView;
     private Button button;
     private MyManage myManage;
+    private LocationManager locationManager;
+    private Criteria criteria;
+    private Double userLatADouble = 13.964987, userLngADouble = 100.585154;
+    private boolean aBoolean = true;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,6 +34,11 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         myManage = new MyManage(MainActivity.this);
+        locationManager = (LocationManager) getSystemService(LOCATION_SERVICE);
+        criteria = new Criteria();
+        criteria.setAccuracy(Criteria.ACCURACY_FINE);
+        criteria.setAltitudeRequired(false);
+        criteria.setBearingRequired(false);
 
         //Bind Widget การผูกตัวแปร
         listView = (ListView) findViewById(R.id.livBusStop);
@@ -53,13 +67,101 @@ public class MainActivity extends AppCompatActivity {
             } // onLongClick
         });
 
+        //My Loop
+        myLoop();
+
+
     }// Main Medthod
+
+    private void myLoop() {
+
+        //Doing
+        afterResume();
+
+
+        //Post
+        if (aBoolean) {
+            Handler handler = new Handler();
+            handler.postDelayed(new Runnable() {
+                @Override
+                public void run() {
+                    myLoop();
+                }
+            }, 1000);
+        }
+
+
+    }   // myLoop
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        locationManager.removeUpdates(locationListener);
+    }
+
+    public Location myFindLocation(String strProvicer) {
+
+        Location location = null;
+        if (locationManager.isProviderEnabled(strProvicer)) {
+            locationManager.requestLocationUpdates(strProvicer, 1000, 10, locationListener);
+            location = locationManager.getLastKnownLocation(strProvicer);
+        }
+
+        return location;
+    }
+
+    public LocationListener locationListener = new LocationListener() {
+        @Override
+        public void onLocationChanged(Location location) {
+            userLatADouble = location.getLatitude();
+            userLngADouble = location.getLongitude();
+        }
+
+        @Override
+        public void onStatusChanged(String s, int i, Bundle bundle) {
+
+        }
+
+        @Override
+        public void onProviderEnabled(String s) {
+
+        }
+
+        @Override
+        public void onProviderDisabled(String s) {
+
+        }
+    };
+
+
 
     @Override
     protected void onResume() {
         super.onResume();
         createListView();
+        afterResume();
     }
+
+    private void afterResume() {
+
+        locationManager.removeUpdates(locationListener);
+
+        Location networkLocation = myFindLocation(LocationManager.NETWORK_PROVIDER);
+        if (networkLocation != null) {
+            userLatADouble = networkLocation.getLatitude();
+            userLngADouble = networkLocation.getLongitude();
+        }
+
+        Location gpsLocation = myFindLocation(LocationManager.GPS_PROVIDER);
+        if (gpsLocation != null) {
+            userLatADouble = gpsLocation.getLatitude();
+            userLngADouble = gpsLocation.getLongitude();
+        }
+
+        Log.d("27febV3", "Lat ==> " + userLatADouble);
+        Log.d("27febV3", "Lng ==> " + userLngADouble);
+
+    }   // afterResume
 
     private void createListView() {
 
